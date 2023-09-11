8912e30ad83dcf07814f09f6e1cfe26280506be5
Minjae Park <jayy.intheworld@gmail.com>
2023-09-11 16:24:00 +0900

    first

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
+import Login from "./pages/Login/Login";
+import JoinDone from "./pages/JoinDone/JoinDone"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/joinDone" element={<JoinDone />}></Route>
       </Routes>
     </BrowserRouter>
   );
