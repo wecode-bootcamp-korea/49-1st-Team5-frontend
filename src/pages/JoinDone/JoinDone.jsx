1ef84a89fa280c7708c376c829b1caf32476abd7
Minjae Park <jayy.intheworld@gmail.com>
2023-09-13 10:50:30 +0900

    login 연결 수정

--- src/pages/JoinDone/JoinDone.jsx
+++ src/pages/JoinDone/JoinDone.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./JoinDone.scss";
 import { Link, useNavigate } from "react-router-dom";
-import banner_square from "../../assets/images/banner_square.png"
+import banner_square from "../../assets/images/banner_square.png";
 // import back from "../../assets/images/back.png"
 
-
 const JoinDone = () => {
- 
-    const navigate = useNavigate();
-    const goToMain = () => {
-      navigate("/")
-    };
-  
-  return(
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate("/login");
+  };
+
+  return (
     <div className="main">
       <div className="joinDoneMain">
         <header className="deadHeader">
@@ -21,18 +19,20 @@ const JoinDone = () => {
           </Link>뒤로 */}
         </header>
         <div className="joinDoneImg">
-          <img src={banner_square} alt="회원가입 완료"/>
+          <img src={banner_square} alt="회원가입 완료" />
           <div>
             <p>회원 가입되었습니다!</p>
             <p>이제 로그인해주세요.</p>
           </div>
         </div>
         <footer>
-          <button type="button" onClick={goToMain}>확인</button>
+          <button type="button" onClick={goToMain}>
+            확인
+          </button>
         </footer>
       </div>
     </div>
   );
 };
 
-export default JoinDone
+export default JoinDone;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import "./Login.scss";
+import logo from "../../assets/images/Logo.png";
+import name from "../../assets/images/logo_wecode.png";
+import { Link, useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = userInfo;
+
+  const handleUserInfo = (event) => {
+    const { name, value } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  let regex = new RegExp(
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+  );
+  const isActiveButton = regex.test(email) && password.length >= 10;
+
+  const goToMain = () => {
+    navigate("/");
+
+    // const JoinToken = localStorage.getItem("JoinToken");
+
+    // if (!JoinToken) {
+    //   alert("회원가입을 먼저 진행해주세요");
+    //   navigate("/JoinInfo");
+    // }
+
+    //////////////////////////
+
+    // fetch("http://10.58.52.134:8000/users/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   },
+    //   body: JSON.stringify({
+    //     email: email,
+    //     password: password,
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     if (result.message === "SUCCESS") {
+    //       localStorage.setItem("token", result.token);
+    //       navigate("/");
+    //     } else if (result.message === >"EMAIL_OR_PASSWORD_ERROR") {
+    //       alert("아이디, 비밀번호를 확인해주세요");
+    //     } else if (result.message === >"PASSWORD_LENGTHERROR") {
+    //       alert("비밀번호를 확인해주세요");
+    //     } else {
+    //       alert("로그인 실패");
+    //     }
+    //   });
+  };
+
+  return (
+    <div className="main">
+      <div className="login">
+        <div className="login_img">
+          <img className="logo" src={logo} alt="위코드 로고" />
+          <img className="name" src={name} alt="위코드 이름" />
+        </div>
+        <form className="login_form">
+          <input
+            type="text"
+            placeholder="이메일"
+            name="email"
+            value={email}
+            onChange={handleUserInfo}
+            required
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            name="password"
+            value={password}
+            onChange={handleUserInfo}
+            required
+          />
+          <button
+            type="button"
+            onClick={goToMain}
+            className={`loginButton ${isActiveButton ? "" : "disabled"}`}
+            disabled={!isActiveButton}
+          >
+            로그인
+          </button>
+        </form>
+        <ul className="search">
+          <li>
+            <Link to="/joininfo">회원가입</Link>
+          </li>
+          <li> 비밀번호 찾기</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
